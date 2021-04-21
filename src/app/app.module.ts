a2701566b30a68546d38c10dcd229d4d40ca2057
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-21 09:00:14 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+//firebase
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
