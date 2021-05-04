dfb6e4d649b9fef43cbdf779d9b4ad7a8eaa03e0
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-04 10:02:44 +0100

    Update table-list.component.ts

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'app/classes/user';
+import { UserService } from 'app/services/user.service';
 
 @Component({
   selector: 'app-table-list',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  user:User;
+  users:User[];
+  b=false;
+  constructor(private userService:UserService) { }
+  
+  ngOnInit(): void {
+    localStorage.clear();
+    this.user=new User();
+    this.read();
   }
+  adduser()
+  {
+ let us=Object.assign({},this.user);
+ this.userService.create_NewUser(us);
+ alert("ajouté avec succés!");
+this.user=new User(); 
+ 
+  }
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
+  
+      this.users = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+         nom: e.payload.doc.data()["nom"],
+         tel: e.payload.doc.data()["tel"],
+         grade: e.payload.doc.data()["grade"],
+         login: e.payload.doc.data()["login"],
+         mdp: e.payload.doc.data()["mdp"],
+         adresse: e.payload.doc.data()["adresse"],
+         ipref: e.payload.doc.data()["ipref"],
+  
+  
+  
+        };
+      });
+      console.log(this.users);
+  
+    });
+  
+  
+  }
+  supprimer(id)
+{
+  if(confirm("vous voulez supprimer l'User?"))
+  this.userService.delete_User(id);
+}
 
 }
